9948aca4705236a6b97ea9a833cacd34b45df819
Maxim Bondarenco <Maxim.Bondarenco@endava.com>
2022-12-10 16:34:48 +0200

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
+type SelectedCard = {
+  rowIndex: number
+  colIndex: number
+  value: number
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [grid] = useState<number[][]>([
+    [5, 1, 2, 3],
+    [3, 2, 4, 1],
+    [0, 4, 0, 5],
+  ])
+  const [selectedCards, setSelectedCards] = useState<SelectedCard[]>([])
+  const [guessedCards, setGuessedCards] = useState<number[]>([])
+  const [numberOfMoves, setNumberOfMoves] = useState<number>(0)
+
+  const handleClick = (rowIndex: number, colIndex: number, value: number) => () => {
+    if (guessedCards.includes(value)) return;
+
+    const temp: SelectedCard = {
+      rowIndex,
+      colIndex,
+      value,
+    }
+
+    if (selectedCards.length < 2) {
+      setSelectedCards(prevState => [...prevState, temp])
+    } else {
+      setSelectedCards([])
+      setSelectedCards(prevState => [...prevState, temp])
+    }
+  }
+
+  useEffect(() => {
+    if (selectedCards[0]?.value === selectedCards[1]?.value) {
+      setGuessedCards(prevState => [...prevState, selectedCards[0]?.value])
+    }
+    if (selectedCards.length === 2 && selectedCards[0]?.value !== selectedCards[1]?.value) {
+      setTimeout(() => {
+        setSelectedCards([])
+      }, 500)
+    }
+    if(selectedCards.length === 2) setNumberOfMoves(numberOfMoves + 1)
+  }, [selectedCards])
+
+  const colConditionIndex = (rowIndex: number, colIndex: number, i: number): boolean => {
+    return selectedCards[i]?.rowIndex === rowIndex && selectedCards[i]?.colIndex === colIndex
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{numberOfMoves}</div>
+      {grid.map((row, rowIndex) => (
+        <div key={rowIndex} className='row'>
+          {row.map((col, colIndex) => (
+            <div
+              key={colIndex}
+              className={`col ${colConditionIndex(rowIndex, colIndex, 0)
+                ? 'selected'
+                : colConditionIndex(rowIndex, colIndex, 1) ? 'selected' :
+                  guessedCards.includes(col) ? 'selected' : ''}`}
+              onClick={handleClick(rowIndex, colIndex, col)}
+            >
+              {colConditionIndex(rowIndex, colIndex, 0)
+                ? col
+                : colConditionIndex(rowIndex, colIndex, 1)
+                  ? col
+                  : guessedCards.includes(col) ? col : ''}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   )
 }
